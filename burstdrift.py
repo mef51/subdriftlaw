e2e3132bd0c18d92b14066fb59600757e48ba606
Mohammed Chamma <hms.mohammed@gmail.com>
2020-04-05 22:26:15 -0400

    autocorrelate and fit a tilted 2d gaussian to the correlation.

--- burstdrift.py
+++ burstdrift.py
@@ -17,76 +17,68 @@ from math import log10
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from scipy import pi as nombrepi
+from scipy import signal
 from tqdm import tqdm
 from matplotlib import colors as mcolors
 import functools
 print = functools.partial(print, flush=True) # print doesn't happen til script ends so force it to flush... windows thing?
 
 burst = 16
-
 print('loading data...')
 junk, nchan, nbin, I, Q, U, V = np.loadtxt('data/{}_puppi_57772_C0531+33_0007_2695.dm559.72.calibP.RM.DD.ASCII'.format(burst), delimiter=' ', unpack=True)
 
 n = len(junk)
 
 binmax = int(nbin[n-1])+1
-
-frequencemax = (int(nchan[n-1])+1)
-
-#~ temps = np.zeros(tempsmax)
-#~ frequence = np.zeros(feqmax)
-
-intensite = np.zeros((frequencemax,binmax))
+frequencymax = (int(nchan[n-1])+1)
+print("dimensions:", binmax, frequencymax)
+intensity = np.zeros((frequencymax,binmax))
 
 X = np.zeros(binmax)
-Y = np.zeros(frequencemax)
+Y = np.zeros(frequencymax)
 
 tmin = 500
 
 tmax = 1500
 
-intensitebruit1 = np.zeros(tmin-1)
-intensitebruit2 = np.zeros(binmax-tmax)
-print("noise removal...")
-for i in tqdm(range(frequencemax-50,51,-1)):
+intensitynoise1 = np.zeros(tmin-1)
+intensitynoise2 = np.zeros(binmax-tmax)
+for i in tqdm(range(frequencymax-50,51,-1), desc='noise removal'):
 
-	Y[i-1] = 4.15 + (i-1) * 1.5625
+	Y[i-1] = 4.15 + (i-1) * 1.5625 # ?
 
 	for j in range(1,tmin) :
 
-		intensitebruit1[j-1] = (I[j-1 + binmax*(frequencemax-i)])/(tmin-1)
+		intensitynoise1[j-1] = (I[j-1 + binmax*(frequencymax-i)])/(tmin-1)
 
 
 	for j in range(tmax+1,binmax+1) :
 
-		intensitebruit2[j-1-tmax] = (I[j-1 + binmax*(frequencemax-i)])/(binmax-tmax)
+		intensitynoise2[j-1-tmax] = (I[j-1 + binmax*(frequencymax-i)])/(binmax-tmax)
 
-	a = sum(intensitebruit1)
-	b = sum(intensitebruit2)
+	a = sum(intensitynoise1)
+	b = sum(intensitynoise2)
 
 	for j in range(1,binmax+1) :
-
 		X[j-1] = j-1
+		intensity[i-1,j-1] = I[j-1 + binmax*(frequencymax-i)] - (a+b)/2
 
-		intensite[i-1,j-1] = I[j-1 + binmax*(frequencemax-i)] - (a+b)/2
-
-# Variance
-print("variance...")
-for i in tqdm(range(52,frequencemax-49)):
-
+for i in tqdm(range(52,frequencymax-49), desc='variance'):
 	for j in range(tmax+1,binmax+1) :
+		intensitynoise2[j-1-tmax] = intensity[i-1,j-1]
 
-		intensitebruit2[j-1-tmax] = intensite[i-1,j-1]
+	mean = np.mean(intensitynoise2)
+	variance = np.var(intensitynoise2)
 
-	moyenne = np.mean(intensitebruit2)
-	variance = np.var(intensitebruit2)
-
+print("finding auto-correlation...")
+corr = signal.correlate2d(intensity[20:30], intensity[20:30], boundary='fill', mode='full')
+print('omg finally finished')
 # Plot
 
-abscisse,ordonnee = np.meshgrid(X,Y)
+# abscisse,ordonnee = np.meshgrid(X,Y)
 
 cmap = "gray"
-
-plotcolor = plt.imshow(intensite,cmap=cmap, interpolation='bicubic',aspect='auto', origin="lower")
+plt.imshow(intensity, cmap=cmap, interpolation='bicubic',aspect='auto', origin="lower")
+plt.imshow(corr, cmap=cmap, interpolation='bicubic',aspect='auto', origin="lower")
 plt.title("Burst #{}".format(burst))
 plt.show()
